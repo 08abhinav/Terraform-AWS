a2f7100b9f583757957a508aba5bd1d9fb87b390
Ubuntu <ubuntu@ip-172-31-26-236.ec2.internal>
2025-11-10 15:42:41 +0000

    feat: added s3 bucket

--- main.tf
+++ main.tf
@@ -71,3 +71,32 @@ resource "aws_security_group" "mysg" {
   }
 }
 
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "terraform-project-bucket-unique2025"
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_ownership" {
+  bucket = aws_s3_bucket.mybucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_access" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "s3_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.s3_ownership,
+    aws_s3_bucket_public_access_block.s3_access,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
