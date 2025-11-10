60295d43e815f88a3cf81cb78ca3b08e6490d442
Ubuntu <ubuntu@ip-172-31-26-236.ec2.internal>
2025-11-10 14:44:14 +0000

    feat: created variables file for using them in main.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "cidr_block" {
+  description = "cidr block for vpc"
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr"{
+  description = "cidr block for subnet1 inside myvpc"
+  default = "10.0.0.0/24"
+}
+
+variable "subnet_cidr2"{
+  description = "cidr block for subnet1 inside myvpc"
+  default = "10.0.1.0/24"
+}
+
+variable "az"{
+  description = "availability zone for subnet1"
+  default = "us-east-1a"
+}
+
+variable "az2"{
+  description = "avaialbility zone for subnet2"
+  default = "us-east-1b"
+}
