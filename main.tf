2d6aff27e9ea4bf4dfee006c5eb749c65a5b5286
Ubuntu <ubuntu@ip-172-31-26-236.ec2.internal>
2025-11-10 15:29:50 +0000

    feat: add security group for inbound and outbound traffic

--- main.tf
+++ main.tf
@@ -38,3 +38,32 @@ resource "aws_route_table_association" "rta2" {
   subnet_id      = aws_subnet.mysubnet2.id
   route_table_id = aws_route_table.RT.id
 }
+
+resource "aws_security_group" "mysg" {
+  name        = "web-sg"
+  vpc_id      = aws_vpc.myvpc.id
+  ingress{
+    description = "HTTP for vpc"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+  }
+  ingress{
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "ssh"
+    cidr_block = ["0.0.0.0/0"]
+  }
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+  }
+  tag = {
+    Name = "Web-sg"
+  }
+}
+
