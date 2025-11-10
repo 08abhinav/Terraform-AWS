d2199ca3268913ea0fa5fd21b9ac228a8acd81c8
08abhinav <125854461+08abhinav@users.noreply.github.com>
2025-11-10 18:50:52 +0000

    feat: added load balancer and target group

--- main.tf
+++ main.tf
@@ -123,6 +123,58 @@ resource "aws_instance" "webserver2" {
   }
 }
 
+resource "aws_lb" "myalb" {
+  name               = "myalb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.mysg.id]
+  subnets         = [aws_subnet.mysubnet1.id, aws_subnet.mysubnet2.id]
+
+  tags = {
+    Name = "web"
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "myTG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach1" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "attach2" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.tg.arn
+    type             = "forward"
+  }
+}
+
+output "loadbalancerdns" {
+  value = aws_lb.myalb.dns_name
+}
+
 output "instance1_public_ip" {
   value = aws_instance.webserver1.public_ip
 }
@@ -130,4 +182,3 @@ output "instance1_public_ip" {
 output "instance2_public_ip" {
   value = aws_instance.webserver2.public_ip
 }
-
