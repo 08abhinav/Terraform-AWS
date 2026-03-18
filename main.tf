29f38bfc1599c4b20d6c02ac92925f088e047d6c
08abhinav <abhinavnegi101@gmail.com>
2026-03-18 16:46:45 +0530

    update: added data section to get ami for ubuntu

--- main.tf
+++ main.tf
@@ -1,125 +1,78 @@
-resource "aws_vpc" "myvpc"{
-  cidr_block = var.cidr_block
-}
+data "aws_ami" "ubuntu"{
+  most_recent = true
+  owners = ["099720109477"]
 
-resource "aws_subnet" "mysubnet1"{
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.subnet_cidr
-  availability_zone = var.az
-  map_public_ip_on_launch = true
-}
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 
-resource "aws_subnet" "mysubnet2"{
-  vpc_id = aws_vpc.myvpc.id
-  cidr_block = var.subnet_cidr2 
-  availability_zone = var.az2
-  map_public_ip_on_launch = true
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.myvpc.id
-}
-
-resource "aws_route_table" "RT" {
-  vpc_id = aws_vpc.myvpc.id
-  
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
-resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.mysubnet1.id
-  route_table_id = aws_route_table.RT.id
+module "network" {
+  source = "./network"
 }
 
-resource "aws_route_table_association" "rta2" {
-  subnet_id      = aws_subnet.mysubnet2.id
-  route_table_id = aws_route_table.RT.id
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_s3_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
 }
 
-resource "aws_security_group" "mysg" {
-  name        = "web-sg"
-  vpc_id      = aws_vpc.myvpc.id
-
-  ingress{
-    description = "HTTP for vpc"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress{
-    description = "SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress{
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "Web-sg"
-  }
+resource "aws_iam_role_policy_attachment" "s3_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "terraform-project-bucket-unique2025"
-}
-
-resource "aws_s3_bucket_ownership_controls" "s3_ownership" {
-  bucket = aws_s3_bucket.mybucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "s3_access" {
-  bucket = aws_s3_bucket.mybucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "s3_acl" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.s3_ownership,
-    aws_s3_bucket_public_access_block.s3_access,
-  ]
-
-  bucket = aws_s3_bucket.mybucket.id
-  acl    = "public-read"
+resource "aws_iam_instance_profile" "profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
 }
 
 resource "aws_instance" "webserver1" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.mysg.id]
-  subnet_id = aws_subnet.mysubnet1.id
-  user_data_base64 = base64encode(file("userdata.sh"))
+  key_name = var.keyPair
+  iam_instance_profile = aws_iam_instance_profile.profile.name
+
+  vpc_security_group_ids = [
+    module.network.security_group_id
+  ]
+
+  subnet_id = module.network.subnet1_id
+  user_data_base64 = base64encode(file("./data/userdata.sh"))
+
   tags = {
-    Name = "instance1"
+    Name = "webserver1"
   }
 }
 
 resource "aws_instance" "webserver2" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.mysg.id]
-  subnet_id = aws_subnet.mysubnet2.id
-  user_data_base64 = base64encode(file("userdata2.sh"))
+  key_name = var.keyPair
+  iam_instance_profile = aws_iam_instance_profile.profile.name
+
+  vpc_security_group_ids = [ module.network.security_group_id ]
+
+  subnet_id = module.network.subnet2_id
+  user_data_base64 = base64encode(file("./data/userdata2.sh"))
+
   tags = {
-    Name = "instance2"
+    Name = "webserver2"
   }
 }
 
@@ -128,8 +81,11 @@ resource "aws_lb" "myalb" {
   internal           = false
   load_balancer_type = "application"
 
-  security_groups = [aws_security_group.mysg.id]
-  subnets         = [aws_subnet.mysubnet1.id, aws_subnet.mysubnet2.id]
+  security_groups = [ module.network.security_group_id ]
+  subnets         = [
+    module.network.subnet1_id, 
+    module.network.subnet2_id
+  ]
 
   tags = {
     Name = "web"
@@ -140,7 +96,7 @@ resource "aws_lb_target_group" "tg" {
   name     = "myTG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.myvpc.id
+  vpc_id   = module.network.vpc_id
 
   health_check {
     path = "/"
@@ -170,6 +126,37 @@ resource "aws_lb_listener" "listener" {
     type             = "forward"
   }
 }
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_ownership" {
+  bucket = aws_s3_bucket.mybucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_access" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "s3_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.s3_ownership,
+    aws_s3_bucket_public_access_block.s3_access,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
+
 
 output "loadbalancerdns" {
   value = aws_lb.myalb.dns_name
