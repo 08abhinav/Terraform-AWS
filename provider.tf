ea059024dd095940f14ad6b7080803299819026f
08abhinav <abhinavnegi101@gmail.com>
2026-03-18 16:46:00 +0530

    provider declaration

--- provider.tf
+++ provider.tf
@@ -1,12 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.20.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket  =  var.bucket_name
+    key     =  "terraform-aws-project-statefile/terraform.tfstate"
+    region  =  "us-east-1"
+    encrypt =  true
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
