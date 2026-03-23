68c8a13d2d29e6f1722fee8c8c3e7beda0bc151d
08abhinav <abhinavnegi101@gmail.com>
2026-03-23 12:43:31 +0530

    updated file

--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
 variable "instance_type"{
   description = "specifying the ram, cpu"
-  default = "t2.micro"
 }
 
 variable "keyPair"{
   description = "key pair for ssh to remote server"
-  default = "shell-practice"
 }
 
-variable "bucket_name"{
-  description = "Bucket name"
-  default = "<replace-with-your-unique-bucket-name>"
+variable "backend_bucket"{
+  descirption = "separate bucket to store the statefile"
 }
